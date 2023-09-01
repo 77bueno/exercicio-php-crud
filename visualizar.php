4811dbd15e55652ee1bbc3b3f704b3e6351f849e
victor bueno <victorhugomotabueno@gmail.com>
2023-09-01 12:02:54 -0300

    exercicio feito hahahahahha

--- visualizar.php
+++ visualizar.php
@@ -12,16 +12,19 @@ $alunos = lerAlunos($conexao);
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Lista de alunos - Exercício CRUD com PHP e MySQL</title>
 <link href="css/style.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
+    <div class="m-5">
+    
     <h1>Lista de alunos</h1>
     <hr>
     <p><a href="inserir.php">Inserir novo aluno</a></p>
 
-   <table border="1">
+   <table class="table">
     <tr>
-        <th> Nome </th>
+        <th scope="col"> Nome </th>
         <th> Primeira Nota </th>
         <th> Segunda Nota </th>
         <th> Média </th>
@@ -32,7 +35,7 @@ $alunos = lerAlunos($conexao);
     
     <?php foreach ($alunos as $aluno) { ?>
     <tr>
-        <td><?=$aluno['nome']?></td>
+        <td  scope="row"><?=$aluno['nome']?></td>
         <td><?=$aluno['primeira_nota']?></td>
         <td><?=$aluno['segunda_nota']?></td>
         <td><?=resultadoMedia($aluno['primeira_nota'], $aluno['segunda_nota'])?></td>
@@ -45,8 +48,10 @@ $alunos = lerAlunos($conexao);
 
 
     <p><a href="index.php">Voltar ao início</a></p>
+    </div>
 </div>
 
 <script src="js/confirmar-exclusao.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
